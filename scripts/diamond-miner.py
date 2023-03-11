65af5df36a8f17e2d2fb7c624553b33e03056945
Ubuntu <ubuntu@ip-172-31-22-16.us-east-2.compute.internal>
2023-03-11 15:00:09 +0000

    update

--- scripts/diamond-miner.py
+++ scripts/diamond-miner.py
@@ -1,3 +1,4 @@
+import sys
 import logging
 from pathlib import Path
 from uuid import uuid4
@@ -26,11 +27,19 @@ measurement_id = str(uuid4())
 probes_filepath = Path("probes.csv.zst")
 results_filepath = Path("results.csv")
 
+target_prefix = sys.argv[1]
+rate = int(sys.argv[2])
+outFileName = sys.argv[3]
+
 # ICMP traceroute towards every /24 in 1.0.0.0/22 starting with 6 flows per prefix between TTLs 2-32
-prefixes = [("104.64.0.0/16", "udp", range(2, 33), 1)]
+prefixes = [(target_prefix, "udp", range(1, 33), 6)]
+def save_file(file_name,content):
+    with open(file_name,'w', encoding='utf-8') as f:
+        f.write('probe_dst_addr\tprobe_src_port\tnear_ttl\tfar_ttl\tnear_addr\tfar_addr\n')
+        f.write(content)
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.WARNING)
     with ClickHouseClient(**credentials) as client:
         CreateTables().execute(client, measurement_id)
         for round_ in range(1, 10):
@@ -71,9 +80,12 @@ if __name__ == "__main__":
             # Send the probes
             config = prober.Config()
             config.set_output_file_csv(str(results_filepath))
-            config.set_probing_rate(10_000)
+            config.set_probing_rate(rate)
             config.set_sniffer_wait_time(1)
             prober.probe(config, str(probes_filepath))
 
         links = GetLinks().execute(client, measurement_id)
+        links_client = client.text('select distinct probe_dst_addr,probe_src_port,near_ttl,far_ttl, \
+                                    near_addr,far_addr from links__{} order by probe_dst_addr'.format(measurement_id).replace("-", "_"))
+        save_file(outFileName,links_client)
         print(f"{len(links)} links discovered")
--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+ulimit -n 65535
+
 ##################### path
 continent=NA
-scripts_path=${HOME}/measurement/scripts
-results_path=${HOME}/measurement/results
-prefix_path=${HOME}/measurement/prefixes
+scripts_path=/home/ubuntu/measurement/scripts
+results_path=/home/ubuntu/measurement/results
+prefix_path=/home/ubuntu/measurement/prefixes
 
 count=0
 while read -r prefix; do
@@ -15,18 +17,18 @@ while read -r prefix; do
 	then
 		echo "dminer twice -- ${count}th prefix -- ${prefix}.dminer12"
 		echo "dminer twice -- ${count}th prefix -- ${prefix}" >> ${results_path}/dminer.log
-		sudo python3 ${scripts_path}/yarrp.py ${prefix} 10000 ${results_path}/${prefix_stripped}.dminer1 &>> ${results_path}/dminer.log
-		sudo python3 ${scripts_path}/yarrp.py ${prefix} 10000 ${results_path}/${prefix_stripped}.dminer2 &>> ${results_path}/dminer.log
+		python3 ${scripts_path}/diamond-miner.py ${prefix} 10000 ${results_path}/${prefix_stripped}.dminer &>> ${results_path}/dminer.log
+		python3 ${scripts_path}/diamond-miner.py ${prefix} 10000 ${results_path}/${prefix_stripped}.dminer2 &>> ${results_path}/dminer.log
 	else
 		echo "dminer once -- ${count}th prefix -- ${prefix}.dminer"
 		echo "dminer once -- ${count}th prefix -- ${prefix}" >> ${results_path}/dminer.log
-		sudo python3 ${scripts_path}/yarrp.py ${prefix} 10000 ${results_path}/${prefix_stripped}.dminer &>> ${results_path}/dminer.log
+		python3 ${scripts_path}/diamond-miner.py ${prefix} 10000 ${results_path}/${prefix_stripped}.dminer &>> ${results_path}/dminer.log
 	fi
 
 
 	echo "zmap -- ${count}th prefix -- ${prefix}.zmap"
 	echo "zmap -- ${count}th prefix -- ${prefix}" >> ${results_path}/zmap.log
-	sudo zmap ${prefix}  -M udp --probe-args=latency:0000 -p 53 -r 10000 -t 210 -c 5 -i eth0 \
+	zmap ${prefix}  -M udp --probe-args=latency:0000 -p 53 -r 10000 -t 210 -c 5 -i eth0 \
 		--output-file=${results_path}/${prefix_stripped}.zmap \
 		--metadata-file=${results_path}/temp.metadata \
 		--output-fields="icmp_responder,saddr,icmp_type,icmp_code,icmp_timestamp,icmp_rtt, origin_ttl" \
@@ -35,7 +37,7 @@ while read -r prefix; do
 
 	echo "zcomplement -- ${count}th prefix -- ${prefix}.zcomplement"
 	echo "zcomplement -- ${count}th prefix -- ${prefix}" >> ${results_path}/zcomplement.log
-	sudo zmap ${prefix}  -M udp --probe-args=latency:0000 -p 100 -r 10000 -t 210 -c 5 -i eth0 \
+	zmap ${prefix}  -M udp --probe-args=latency:0000 -p 100 -r 10000 -t 210 -c 5 -i eth0 \
 		--output-file=${results_path}/${prefix_stripped}.zcomplement \
 		--metadata-file=${results_path}/temp.metadata \
 		--output-fields="icmp_responder,saddr,icmp_type,icmp_code,icmp_timestamp,icmp_rtt, origin_ttl" \
