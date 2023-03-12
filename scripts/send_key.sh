78934f12adddaf86371bb0147a4d1b96269b5eaf
qi <lw020806@sjtu.edu.cn>
2023-03-12 00:52:05 -0500

    update

---
+++ scripts/send_key.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+targetIp=$1
+sshFile=$2
+
+if [[ -z "${sshFile}" ]]; then
+	echo "empty target"
+	exit 1
+fi
+
+cp ~/.aws/${sshFile} ~/.ssh/
+chmod 400 ~/.ssh/${sshFile}
+scp -i ~/.ssh/${sshFile} /home/qi/.ssh/Ohio-rsa.pem ubuntu@${targetIp}:/home/ubuntu/.ssh/
