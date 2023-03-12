34c1718aa32808763ff298f635a0b38dc4626de4
qi <lw020806@sjtu.edu.cn>
2023-03-11 20:06:09 -0500

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,17 @@ pip3 install diamond-miner pycaracal pych-client
 
 python3 -m site | grep "site-packages"
 echo "ATTENTION: modify -- filter_private: bool = False"
+sed -i "s/filter_private: bool = True/filter_private: bool = False/" /usr/local/lib/python3.10/dist-packages/diamond_miner/queries/query.py
 
 apt install -y docker
 apt install -y docker.io
 docker run --rm -d -p 8123:8123 clickhouse/clickhouse-server:22.6
+
+cp results_template results -r
+mkdir results_zip
+
+chmod 400 /home/ubuntu/.ssh/Ohio-rsa.pem
+scp -i /home/ubuntu/.ssh/Ohio-rsa.pem README.md  ubuntu@3.142.185.31:/home/ubuntu/collections/
+
+ulimit -n 65535
+ulimit -a
--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -13,32 +13,34 @@ scripts_path=/home/ubuntu/network-measurements/scripts
 results_path=/home/ubuntu/network-measurements/results
 zip_path=/home/ubuntu/network-measurements/results_zip
 prefix_path=/home/ubuntu/network-measurements/results
+log_file=/home/ubuntu/network-measurements/log
+myIp=3.142.185.31
 
 count=0
 while read -r prefix12; do
 	prefix12_stripped="${prefix12//\//_}"
-	echo "SCAN ${prefix12}"
+	echo "SCAN ${prefix12}" >> ${log_file}
 
 	while read -r prefix16; do
 		count=$(( $count + 1 ))
-		echo "\tSCAN ${prefix16}"
+		echo -e "\tSCAN ${prefix16}" >> ${log_file}
 
 		prefix16_stripped="${prefix16//\//_}"
 		results_path16=${results_path}/${continent}/${prefix12_stripped}
 		if [ $(( $count % 10 )) == 0 ]
 		then
-			echo "\tDMINER twice -- ${count}th prefix -- ${prefix16}.dminer12"
+			echo -e "\tDMINER twice -- ${count}th prefix -- ${prefix16}.dminer12" >> ${log_file}
 			echo "DMINER twice -- ${count}th prefix -- ${prefix16}.dminer12" >> ${results_path16}/dminer.log
 			python3 ${scripts_path}/diamond-miner.py ${prefix16} 10000 ${results_path16}/${prefix16_stripped}.dminer1 &>> ${results_path16}/dminer.log
 			python3 ${scripts_path}/diamond-miner.py ${prefix16} 10000 ${results_path16}/${prefix16_stripped}.dminer2 &>> ${results_path16}/dminer.log
 		else
-			echo "\tDMINER once -- ${count}th prefix -- ${prefix16}.dminer"
+			echo -e "\tDMINER once -- ${count}th prefix -- ${prefix16}.dminer" >> ${log_file}
 			echo "DMINER once -- ${count}th prefix -- ${prefix16}.dminer" >> ${results_path16}/dminer.log
 			python3 ${scripts_path}/diamond-miner.py ${prefix16} 10000 ${results_path16}/${prefix16_stripped}.dminer &>> ${results_path16}/dminer.log
 		fi
 
 
-		echo "\tZMAP -- ${count}th prefix -- ${prefix16}.zmap"
+		echo -e "\tZMAP -- ${count}th prefix -- ${prefix16}.zmap" >> ${log_file}
 		echo "ZMAP -- ${count}th prefix -- ${prefix16}" >> ${results_path16}/zmap.log
 		zmap ${prefix16}  -M udp --probe-args=latency:0000 -p 53 -r 10000 -t 210 -c 5 -i eth0 \
 			--output-file=${results_path16}/${prefix16_stripped}.zmap \
@@ -47,7 +49,7 @@ while read -r prefix12; do
 			&>>${results_path16}/zmap.log
 		cat ${results_path16}/temp.metadata >> ${results_path16}/zmap.metadata
 
-		echo "\tZCOMPLEMENT -- ${count}th prefix -- ${prefix16}.zcomplement"
+		echo -e "\tZCOMPLEMENT -- ${count}th prefix -- ${prefix16}.zcomplement" >> ${log_file}
 		echo "ZCOMPLEMENT -- ${count}th prefix -- ${prefix16}.zcomplement" >> ${results_path16}/zcomplement.log
 		zmap ${prefix16}  -M udp --probe-args=latency:0000 -p 100 -r 10000 -t 210 -c 5 -i eth0 \
 			--output-file=${results_path16}/${prefix16_stripped}.zcomplement \
@@ -56,12 +58,14 @@ while read -r prefix12; do
 			&>>${results_path16}/zcomplement.log
 		cat ${results_path16}/temp.metadata >> ${results_path16}/zcomplement.metadata
 
-		echo "\tDONE ${prefix16}"
-		echo "------------------------------------------------"
+		echo -e "\tDONE ${prefix16}" >> ${log_file}
+		echo "------------------------------------------------" >> ${log_file}
 	done <${prefix_path}/${continent}/${prefix12_stripped}/16.prefixes
 
 	# cleanup for prefix12
 	rm ${results_path}/${continent}/${prefix12_stripped}/temp.metadata
 	zip -r ${zip_path}/${prefix12_stripped}.zip ${results_path}/${continent}/${prefix12_stripped}
-	echo "DONE ${prefix12}\n"
+	scp -i /home/ubuntu/.ssh/Ohio-rsa.pem ${zip_path}/${prefix12_stripped}.zip ubuntu@${myIp}:/home/ubuntu/collections/${continent}/ &
+	echo "DONE ${prefix12}\n" >> ${log_file}
+	scp -i /home/ubuntu/.ssh/Ohio-rsa.pem ${log_file} ubuntu@${myIp}:/home/ubuntu/collections/${continent}/ &
 done <${prefix_path}/${continent}/12.prefixes
