15f164a4e9399fb801a6762ac58c678d0e4f2c57
Ubuntu <ubuntu@ip-172-31-22-16.us-east-2.compute.internal>
2023-03-12 19:36:34 +0000

    update

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -19,33 +19,29 @@ myIp=3.142.185.31
 count=0
 while read -r prefix12; do
 	prefix12_stripped="${prefix12//\//_}"
-	echo -n `date +%R` >> ${log_file}
-	echo "SCAN ${prefix12}" >> ${log_file}
+	echo -n `TZ=America/Detroit date +%R` >> ${log_file}
+	echo -e "\tSCAN ${prefix12}" >> ${log_file}
 
 	while read -r prefix16; do
 		count=$(( $count + 1 ))
-		echo -n `date +%R` >> ${log_file}
-		echo -e "\tSCAN ${prefix16}" >> ${log_file}
+		echo -n `TZ=America/Detroit date +%R` >> ${log_file}
+		echo -e "\t\tSCAN ${prefix16}" >> ${log_file}
 
 		prefix16_stripped="${prefix16//\//_}"
 		results_path16=${results_path}/${continent}/${prefix12_stripped}
-		if [ $(( $count % 10 )) == 0 ]
-		then
-			echo -n `date +%R` >> ${log_file}
-			echo -e "\tDMINER twice -- ${count}th prefix -- ${prefix16}.dminer12" >> ${log_file}
-			echo "DMINER twice -- ${count}th prefix -- ${prefix16}.dminer12" >> ${results_path16}/dminer.log
-			python3 ${scripts_path}/diamond-miner.py ${prefix16} 10000 ${results_path16}/${prefix16_stripped}.dminer1 &>> ${results_path16}/dminer.log
-			python3 ${scripts_path}/diamond-miner.py ${prefix16} 10000 ${results_path16}/${prefix16_stripped}.dminer2 &>> ${results_path16}/dminer.log
-		else
-			echo -n `date +%R` >> ${log_file}
-			echo -e "\tDMINER once -- ${count}th prefix -- ${prefix16}.dminer" >> ${log_file}
-			echo "DMINER once -- ${count}th prefix -- ${prefix16}.dminer" >> ${results_path16}/dminer.log
-			python3 ${scripts_path}/diamond-miner.py ${prefix16} 10000 ${results_path16}/${prefix16_stripped}.dminer &>> ${results_path16}/dminer.log
-		fi
 
+		echo -n `TZ=America/Detroit date +%R` >> ${log_file}
+		echo -e "\t\tDMINER one -- ${count}th prefix -- ${prefix16}.dminer1" >> ${log_file}
+		echo "DMINER one -- ${count}th prefix -- ${prefix16}.dminer1" >> ${results_path16}/dminer.log
+		python3 ${scripts_path}/diamond-miner.py ${prefix16} 100000 ${results_path16}/${prefix16_stripped}.dminer1 &>> ${results_path16}/dminer.log
 
-		echo -n `date +%R` >> ${log_file}
-		echo -e "\tZMAP -- ${count}th prefix -- ${prefix16}.zmap" >> ${log_file}
+		echo -n `TZ=America/Detroit date +%R` >> ${log_file}
+		echo -e "\t\tDMINER two -- ${count}th prefix -- ${prefix16}.dminer2" >> ${log_file}
+		echo "DMINER two -- ${count}th prefix -- ${prefix16}.dminer2" >> ${results_path16}/dminer.log
+		python3 ${scripts_path}/diamond-miner.py ${prefix16} 100000 ${results_path16}/${prefix16_stripped}.dminer2 &>> ${results_path16}/dminer.log
+
+		echo -n `TZ=America/Detroit date +%R` >> ${log_file}
+		echo -e "\t\tZMAP -- ${count}th prefix -- ${prefix16}.zmap" >> ${log_file}
 		echo "ZMAP -- ${count}th prefix -- ${prefix16}" >> ${results_path16}/zmap.log
 		zmap ${prefix16}  -M udp --probe-args=latency:0000 -p 53 -r 10000 -t 210 -c 5 -i eth0 \
 			--output-file=${results_path16}/${prefix16_stripped}.zmap \
@@ -54,26 +50,25 @@ while read -r prefix12; do
 			&>>${results_path16}/zmap.log
 		cat ${results_path16}/temp.metadata >> ${results_path16}/zmap.metadata
 
-		echo -n `date +%R` >> ${log_file}
-		echo -e "\tZCOMPLEMENT -- ${count}th prefix -- ${prefix16}.zcomplement" >> ${log_file}
+		echo -n `TZ=America/Detroit date +%R` >> ${log_file}
+		echo -e "\t\tZCOMPLEMENT -- ${count}th prefix -- ${prefix16}.zcomplement" >> ${log_file}
 		echo "ZCOMPLEMENT -- ${count}th prefix -- ${prefix16}.zcomplement" >> ${results_path16}/zcomplement.log
 		zmap ${prefix16}  -M udp --probe-args=latency:0000 -p 100 -r 10000 -t 210 -c 5 -i eth0 \
 			--output-file=${results_path16}/${prefix16_stripped}.zcomplement \
 			--metadata-file=${results_path16}/temp.metadata \
 			--output-fields="icmp_responder,saddr,icmp_type,icmp_code,icmp_timestamp,icmp_rtt, origin_ttl" \
-			&>>${results_path16}/zcomplement.log
-		cat ${results_path16}/temp.metadata >> ${results_path16}/zcomplement.metadata
+			&>>${results_path16}/zmap.log
+		cat ${results_path16}/temp.metadata >> ${results_path16}/zmap.metadata
 
-		echo -n `date +%R` >> ${log_file}
-		echo -e "\tDONE ${prefix16}" >> ${log_file}
-		echo "------------------------------------------------" >> ${log_file}
+		echo -n `TZ=America/Detroit date +%R` >> ${log_file}
+		echo -e "\t\tDONE ${prefix16}" >> ${log_file}
+		echo -e "\t\t------------------------------------------------" >> ${log_file}
 	done <${prefix_path}/${continent}/${prefix12_stripped}/16.prefixes
 
 	# cleanup for prefix12
 	rm ${results_path}/${continent}/${prefix12_stripped}/temp.metadata
-	zip -r ${zip_path}/${prefix12_stripped}.zip ${results_path}/${continent}/${prefix12_stripped}
-	scp -i /home/ubuntu/.ssh/Ohio-rsa.pem ${zip_path}/${prefix12_stripped}.zip ubuntu@${myIp}:/home/ubuntu/collections/${continent}/ &
-	echo -n `date +%R` >> ${log_file}
-	echo "DONE ${prefix12}\n" >> ${log_file}
+	tar -czf ${zip_path}/${prefix12_stripped}.tar.gz ${results_path}/${continent}/${prefix12_stripped} --remove-files &
+	echo -n `TZ=America/Detroit date +%R` >> ${log_file}
+	echo -e "\tDONE ${prefix12}\n" >> ${log_file}
 	scp -i /home/ubuntu/.ssh/Ohio-rsa.pem ${log_file} ubuntu@${myIp}:/home/ubuntu/collections/${continent}/ &
 done <${prefix_path}/${continent}/12.prefixes
