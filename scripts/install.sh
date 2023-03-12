f6b15227b5b13937a0b0cc9baf298d34f9459206
Ubuntu <ubuntu@ip-172-31-22-16.us-east-2.compute.internal>
2023-03-12 19:25:10 +0000

    udpate targets

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,19 +3,17 @@
 apt update
 
 cd zmap
-apt-get install build-essential cmake libgmp3-dev gengetopt libpcap-dev flex byacc libjson-c-dev pkg-config libunistring-dev
+apt-get install -y build-essential cmake libgmp3-dev gengetopt libpcap-dev flex byacc libjson-c-dev pkg-config libunistring-dev
 cmake .
 make -j4
 make install
 cd ..
 
 # dminer
-apt install python3-pip
+apt install -y python3-pip
 pip3 install diamond-miner pycaracal pych-client
-# pip3 install 'pych-client<0.3.0,>=0.2.3'
 
 python3 -m site | grep "site-packages"
-echo "ATTENTION: modify -- filter_private: bool = False"
 sed -i "s/filter_private: bool = True/filter_private: bool = False/" /usr/local/lib/python3.10/dist-packages/diamond_miner/queries/query.py
 
 apt install -y docker
@@ -26,7 +24,7 @@ cp results_template results -r
 mkdir results_zip
 
 chmod 400 /home/ubuntu/.ssh/Ohio-rsa.pem
-scp -i /home/ubuntu/.ssh/Ohio-rsa.pem README.md  ubuntu@3.142.185.31:/home/ubuntu/collections/
+scp -i /home/ubuntu/.ssh/Ohio-rsa.pem README.md ubuntu@3.142.185.31:/home/ubuntu/collections/trash/
 
 ulimit -n 65535
 ulimit -a
