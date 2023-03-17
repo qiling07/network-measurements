1662b408f459b6af80378f0b02ccbd11431bf9fc
Ubuntu <ubuntu@ip-172-31-22-16.us-east-2.compute.internal>
2023-03-17 06:34:01 +0000

    upate

--- scripts/cal-coverage-zmap.py
+++ scripts/cal-coverage-zmap.py
@@ -1,36 +1,42 @@
 import pandas as pd
 import sys
 
-def update_dict(d, ip, ttl) :
-    if ip in d.keys() :
-        if ttl < d[ip] :
+
+def update_dict(d, ip, ttl):
+    if ip in d.keys():
+        if ttl < d[ip]:
             d[ip] = ttl
-    else :
+    else:
         d[ip] = ttl
 
-def add_dict(d, ttl, N) :
-    if ttl in d.keys() :
+
+def add_dict(d, ttl, N):
+    if ttl in d.keys():
         d[ttl] += N
-    else :
+    else:
         d[ttl] = N
+
 
 def count_response_rate(filename):
     responders = {}
     d_data = pd.read_csv(filename, sep=',')
     d_data = d_data.query("icmp_responder == saddr")
-    for i in range(len(d_data)) :
-        update_dict(responders, d_data.saddr[i], d_data.origin_ttl[i])
+    for i in range(len(d_data)):
+        update_dict(
+            responders, d_data.iloc[i].saddr, d_data.iloc[i].origin_ttl)
     return responders
 
-def classify_by_ttl(responders) :
+
+def classify_by_ttl(responders):
     ttls = {}
-    for ip, ttl in responders.items() :
-        update_dict(ttls, ttl, 1)
-    return sorted(ttls.items(), key= lambda item : item[0])
+    for ip, ttl in responders.items():
+        add_dict(ttls, ttl, 1)
+    return sorted(ttls.items(), key=lambda item: item[0])
+
 
 responders = count_response_rate(sys.argv[1])
 print("responder count: " + str(len(responders)) + " out of /16")
 ttls = classify_by_ttl(responders=responders)
-for ttl, cnt in ttls :
-    print(ttl, cnt)
-
+print("ttl\tcnt")
+for ttl, cnt in ttls:
+    print(str(ttl) + "\t" + str(cnt))
