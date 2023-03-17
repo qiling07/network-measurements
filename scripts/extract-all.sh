6150b101a8c174199387bd8ea5b067fabab23286
Ubuntu <ubuntu@ip-172-31-22-16.us-east-2.compute.internal>
2023-03-17 17:18:36 +0000

    update

--- scripts/extract-all.sh
+++ scripts/extract-all.sh
@@ -6,7 +6,7 @@ zip_path=/home/ubuntu/network-measurements/results_zip
 prefix_path=/home/ubuntu/network-measurements/results
 log_file=/home/ubuntu/network-measurements/log2
 
-mkdir ${results_path}/${continent}
+mkdir ${results_path}
 
 while read -r prefix12; do
 	prefix12_stripped="${prefix12//\//_}"
@@ -14,13 +14,13 @@ while read -r prefix12; do
 	echo -e "\tAnalyzing ${prefix12}" >> ${log_file}
 	tar -xzf ${zip_path}/${prefix12_stripped}.tar.gz -C ${zip_path}
 
-	mkdir -p ${results_path}/${continent}/${prefix12_stripped}
+	mkdir -p ${results_path}/${prefix12_stripped}
 	while read -r prefix16; do
 		prefix16_stripped="${prefix16//\//_}"
 		echo -n -e "\t\t${prefix16}: " >> ${log_file}
 		python3 scripts/cal-coverage-dminer.py ${zip_path}/${prefix12_stripped}/${prefix16_stripped}.dminer1 \
 			${zip_path}/${prefix12_stripped}/${prefix16_stripped}.dminer2 \
-			${results_path}/${continent}/${prefix12_stripped}/${prefix16_stripped}.links >> ${log_file}
+			${results_path}/${prefix12_stripped}/${prefix16_stripped}.links >> ${log_file}
 	done <${zip_path}/${prefix12_stripped}/16.prefixes
 
 	rm ${zip_path}/${prefix12_stripped} -r
